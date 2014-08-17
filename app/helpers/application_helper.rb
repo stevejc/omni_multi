ba28662312cc8ad2ab1540676d6e11ef64f9cc1d
Steve <stevemargc@gmail.com>
2014-08-16 20:30:54 -0400

    Added bootstrap styling, updated application page, added header and error partials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def page_title(title)
+    title == "" ? "Multi" : title
+  end
+  
+  def flash_class(type)
+    case type
+    when :alert
+      "alert-danger"
+    when :notice
+      "alert-success"
+    else
+      ""
+    end
+  end
 end
